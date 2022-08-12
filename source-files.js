fad4ddb52f2d6cf218d13301121767d61e2aad1e
yescallop <yescallop@users.noreply.github.com>
2022-08-12 05:53:53 +0000

    deploy: 3f008f8f5cf631b1cb21e898d140602772d0c5b9

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"fluent_uri":["",[["enc",[],["estring.rs","imp.rs","mod.rs","table.rs"]]],["fmt.rs","internal.rs","lib.rs","parser.rs","view.rs"]]\
+"fluent_uri":["",[["enc",[],["encoder.rs","estring.rs","imp.rs","mod.rs","table.rs"]]],["fmt.rs","internal.rs","lib.rs","parser.rs","view.rs"]]\
 }');
 createSourceSidebar();
