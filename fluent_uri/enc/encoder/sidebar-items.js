8092b72a290b76e06bb96af8d3274154ad01ea44
yescallop <yescallop@users.noreply.github.com>
2022-08-30 03:42:33 +0000

    deploy: f0a0536782ba5be66ad2be0c313343517be8ba43

--- fluent_uri/enc/encoder/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":[["PathEncoder","An encoder for the path component."],["QueryFragmentEncoder","An encoder for the query or the fragment component."]],"trait":[["Encoder","A trait used by `EString` to specify the table used for encoding."]]};
--- implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"fluent_uri":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"fluent_uri/struct.ParseError.html\" title=\"struct fluent_uri::ParseError\">ParseError</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
